409731d252d7fe21fba9235b181c362aacfe24a4
AndriiNez <andreynezen@gmail.com>
2023-09-14 10:16:48 +0200

    commit

--- src/main/java/ua/homework/Test.java
+++ src/main/java/ua/homework/Test.java
@@ -4,10 +4,10 @@ import com.google.gson.Gson;
 
 public class Test {
     public static void main(String[] args) {
-    User user = new User("Andrii", "Nezhynskyi");
-    Gson gson = new Gson();
-    String json = gson.toJson(user);
+        User user = new User("Andrii", "Nezhynskyi");
+        Gson gson = new Gson();
+        String json = gson.toJson(user);
 
-    System.out.println(json);
-}
+        System.out.println(json);
+    }
 }
